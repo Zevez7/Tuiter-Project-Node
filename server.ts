d2f5121a5a1b28d12bdaba3267638f8717b7b70d
Zevez7 <datguy7@gmail.com>
2022-12-01 20:16:34 -0500

    added auth to node route

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ import BookmarkController from "./controllers/BookmarkController";
 import MessageController from "./controllers/MessageController";
 import LikeController from "./controllers/LikeController";
 import ProfileController from "./controllers/ProfileController";
-
+import AuthController from "./controllers/auth-controller";
 const cors = require("cors");
 
 const session = require("express-session");
@@ -48,6 +48,7 @@ const messageController = MessageController.getInstance(app);
 const likesController = LikeController.getInstance(app);
 
 const profileController = ProfileController.getInstance(app);
+const authController = AuthController.getInstance(app);
 
 const PORT: any = process.env.PORT || 5000;
 
