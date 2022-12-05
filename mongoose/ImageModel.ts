4aab24bafa47350721d4a6b971ce9e1e500e36af
ujjvalksharma <ujjvalksharma@gmail.com>
2022-12-05 12:54:37 -0500

    Implemented save image service

--- mongoose/ImageModel.ts
+++ mongoose/ImageModel.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import ImageSchema from "../schemas/ImageSchema";
+import ImageSchema from "./ImageSchema";
 
-const imageModel = mongoose.model('ImageModel', ImageModel);
+const imageModel = mongoose.model('ImageModel', ImageSchema);
 export default imageModel;
