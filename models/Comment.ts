5637b972e3b87e94242b5f25a75d934f4120b79d
ujjvalksharma <ujjvalksharma@gmail.com>
2022-12-07 01:08:35 -0500

    Created comment model

---
+++ models/Comment.ts
@@ -0,0 +1,13 @@
+import User from "./User";
+import Tuit from "./Tuit";
+/**
+ * @file Like represents the tuit liked by a user
+ * @property {string} likedBy represents the person who liked the tuit
+ * @property {string} likedTuit represents the tuit which got liked
+ * @property {string} postedOn represents date when tuit was liked by that person
+ */
+export default class Comment {
+    private commentedBy: User | null = null;
+   private commentedTuit: Tuit | null = null;
+   private postedOn: Date = new Date(); 
+} 
