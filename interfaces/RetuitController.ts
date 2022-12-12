84028a9b26f4276a92ca4391a68bf0f830a5d1ed
ujjvalksharma <ujjvalksharma@gmail.com>
2022-12-12 03:23:12 -0500

    Added Retuit controller interface

---
+++ interfaces/RetuitController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express";
+/**
+ * @file This acts an interface for retuit api
+ */
+export default interface RetuitController {
+    /**
+     * Retuit a tuit
+     * @param req request object
+     * @param res response object
+     */
+    RetuitATuit(req: Request, res: Response): void;
+     /**
+     * Get users who retuited a tuit
+     * @param req request object
+     * @param res response object
+     */
+    findUsersThatRetuitATuid(req: Request, res: Response): void;
+}
+
+
